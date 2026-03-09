42b41ba0eeda9f4a96eda8c30f8d008a7a48ab46
sulman01 <sulman01@student.ubc.ca>
2026-03-09 15:23:39 -0700

    save raw data to a file

--- scripts/1_download-data.py
+++ scripts/1_download-data.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-06-22/parks.csv"
 data_raw = pd.read_csv(url)
+data_raw.to_csv("data/raw/parks_raw.csv", index=False)
 
 # verify the script worked
 # print(data_raw.head())
