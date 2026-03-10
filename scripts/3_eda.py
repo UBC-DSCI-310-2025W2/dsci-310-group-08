808fa82d4162271fde7329a45dea758a3ec4a826
sulman01 <sulman01@student.ubc.ca>
2026-03-09 17:30:28 -0700

    figure 1 created

--- scripts/3_eda.py
+++ scripts/3_eda.py
@@ -14,9 +14,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 Path("../artifacts").mkdir(parents=True, exist_ok=True)
 Path("../artifacts/eda").mkdir(parents=True, exist_ok=True)
 
-# # make first plot
-# plt.hist(y_train, bins = 12, edgecolor='black')
-# plt.xlabel("Rank")
-# plt.ylabel("Frequency")
-# plt.title("Figure 1: Ordinal Rank vs Value Frequency in the Training Set");
-# plt.savefig("../artifacts/eda/rank_frequency.png")
+# make first plot
+plt.hist(y_train, bins = 12, edgecolor='black')
+plt.xlabel("Rank")
+plt.ylabel("Frequency")
+plt.title("Figure 1: Ordinal Rank vs Value Frequency in the Training Set");
+plt.savefig("../artifacts/eda/rank_frequency.png")
