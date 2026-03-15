19c7d075d06c6c0532c6d390cedd02b321c2af31
sulman01 <sulman01@student.ubc.ca>
2026-03-14 17:36:34 -0700

    adding click commands to script 1

--- scripts/01_download-data.py
+++ scripts/01_download-data.py
@@ -1,11 +1,28 @@
 # imports
 import pandas as pd
+import click
 from pathlib import Path
 
-# read in raw data
-url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-06-22/parks.csv"
-data_raw = pd.read_csv(url)
+@click.command()
+# add option to specify input path
+@click.option(
+    "--input_path",
+    default="https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2021/2021-06-22/parks.csv"
+)
+# add option to specify output path
+@click.option(
+    "--output_path",
+    default="../data/raw/parks_raw.csv"
+)
 
-# create path to store raw data
-Path("../data/raw").mkdir(parents=True, exist_ok=True)
-data_raw.to_csv("../data/raw/parks_raw.csv", index=False)
+def download_data(input_path, output_path):
+    # read in raw data
+    data_raw = pd.read_csv(input_path)
+
+    # create path to store raw data
+    output_path = Path(output_path)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    data_raw.to_csv(output_path, index=False)
+
+if __name__ == "__main__":
+    download_data()
