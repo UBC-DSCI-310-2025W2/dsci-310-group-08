9fc5e61cf0b601b3bb7dc252438a6bcbd97a5a02
sulman01 <sulman01@student.ubc.ca>
2026-03-14 18:20:26 -0700

    click commands added to script 6

--- scripts/06_results.py
+++ scripts/06_results.py
@@ -1,19 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import click
 from pathlib import Path
 
-# import predictions for plot visualization
-y_preds_df = pd.read_csv("../data/processed/predictions/test_predictions.csv")
-y_pred = y_preds_df["y_pred"]
-y_test = y_preds_df["y_true"]
+@click.command()
+@click.option(
+    "--predictions_path",
+    default="../data/processed/predictions/test_predictions.csv"
+)
+@click.option(
+    "--outputs_path",
+    default="../outputs/results"
+)
+@click.option(
+    "--fig_name",
+    default="04_actual-vs-predicted.png"
+)
 
-# make figure 4 - accuracy plot
-plt.scatter(y_test, y_pred, alpha=0.5)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # perfect prediction line
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
-plt.title('Figure 4: Actual vs Predicted Ranks')
+def results(predictions_path, outputs_path, fig_name):
+    # import predictions for plot visualization
+    y_preds_df = pd.read_csv(predictions_path)
+    y_pred = y_preds_df["y_pred"]
+    y_test = y_preds_df["y_true"]
+    
+    # make figure 4 - accuracy plot
+    plt.scatter(y_test, y_pred, alpha=0.5)
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # perfect prediction line
+    plt.xlabel('Actual')
+    plt.ylabel('Predicted')
+    plt.title('Figure 4: Actual vs Predicted Ranks')
+    
+    # make directory to save the plot
+    outputs_path = Path(outputs_path)
+    outputs_path.mkdir(parents=True, exist_ok=True)
+    plt.savefig(outputs_path / fig_name)
 
-# make directory to save the plot
-Path("../outputs/results").mkdir(parents=True, exist_ok=True)
-plt.savefig("../outputs/results/04_actual-vs-predicted.png")
+if __name__ == "__main__":
+    results()
