025b9ccfc4a33204c065f57b47881e7f3191a252
Shivani Aggarwal <shivani05aggarwal@gmail.com>
2026-03-20 21:36:05 -0700

    removed chart title

--- scripts/06_results.py
+++ scripts/06_results.py
@@ -22,16 +22,15 @@ from pathlib import Path
 
 def results(predictions_path, outputs_path, fig_name):
     # import predictions for plot visualization
-    y_preds_df = pd.read_csv(predictions_path)
+    y_preds_df = pd.read_csv(predictions_path / "test_predictions.csv")
     y_pred = y_preds_df["y_pred"]
     y_test = y_preds_df["y_true"]
     
     # make figure 4 - accuracy plot
     plt.scatter(y_test, y_pred, alpha=0.5)
     plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # perfect prediction line
-    plt.xlabel('Actual')
-    plt.ylabel('Predicted')
-    plt.title('Figure 4: Actual vs Predicted Ranks')
+    plt.xlabel('Actual Ranks')
+    plt.ylabel('Predicted Ranks')
     
     # make directory to save the plot
     outputs_path = Path(outputs_path)
