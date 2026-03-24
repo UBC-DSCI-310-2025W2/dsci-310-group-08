888d20afeaa6e08aa97f08fc094f21f45737776a
sulman01 <sulman01@student.ubc.ca>
2026-03-24 14:24:18 -0700

    docstring created

--- src/create_directory.py
+++ src/create_directory.py
@@ -1,2 +1,14 @@
-def create_directory():
+def create_directory(name, parent):
+    """
+    Creates a directory with the given name, located inside the given parent directory.
+
+    Returns an error if the directory name is illegal.
+    Returns an error if the specified parent directory does not exist.
+
+    Arguments:
+    Name:  desired name of directory.
+    Parent:  desired parent directory of directory to be created.
+    Returns:
+    Path to the newly created directory
+    """
     return None
