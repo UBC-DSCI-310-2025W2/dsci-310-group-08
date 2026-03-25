9b7d5bd2a15f0866abbfb52023fc0d552c6ec217
sulman01 <sulman01@student.ubc.ca>
2026-03-24 20:24:42 -0700

    drafting out simple test cases

--- tests/create_directory.py
+++ tests/create_directory.py
@@ -1,1 +1,16 @@
 import pytest 
+
+def test_create_dir_from_root_dir():
+    """"
+    Tests the use case of creating a directory from the root directory.
+    Expected outcome:  directory created successfully.
+    """
+    return None
+
+def test_create_dir_two_deep():
+    """
+    Tests the use case of creating a subdirectory of an already existing parent directory.  
+    This parent directory should live at the root directory.
+    Expected outcome:  directory created successfully.
+    """"
+    return None
