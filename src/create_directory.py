ae81f0b834c6790b0e6125bb1043d7e7661760d4
sulman01 <sulman01@student.ubc.ca>
2026-03-25 11:46:00 -0700

    error use case tests have been written and are failing

--- src/create_directory.py
+++ src/create_directory.py
@@ -2,12 +2,13 @@ def create_directory(name, parent):
     """
     Creates a directory with the given name, located inside the given parent directory.
 
-    Returns an error if the directory name is illegal.
+    Returns an error if the specified directory name is empty.
+    Returns an error if the specified directory name is illegal.
     Returns an error if the specified parent directory does not exist.
 
     Arguments:
-    Name:  desired name of directory.
-    Parent:  string of desired parent directory of directory to be created.
+    Name: desired name of directory.
+    Parent: string of desired parent directory of directory to be created.
     Returns:
     Path to the newly created directory
     """
